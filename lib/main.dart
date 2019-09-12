106307be484cc062c8a789d25a505e41362aedcc
jgarza9788 <jgarza9788@gmail.com>
2019-09-12 16:03:09 -0700

    add theme widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+      ),
       home: InputPage(),
     );
   }
