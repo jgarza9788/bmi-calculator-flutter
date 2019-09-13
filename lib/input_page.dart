b2cea6cf9e66626d66df99d4dd348afb31979d6c
jgarza9788 <jgarza9788@gmail.com>
2019-09-12 17:21:30 -0700

    refactor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -7,13 +7,75 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   @override
+
   Widget build(BuildContext context) {
+
+    double ThisMargin = 5.0;
+    double roundedCorners = 10.0;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+//      body: Container(
+//        margin: EdgeInsets.all(ThisMargin),
+//        decoration: BoxDecoration(
+//          borderRadius: BorderRadius.circular(20.0),
+//          color: Color(0xFF1D1E33),
+//        ),
+//      ),
+    body: Column(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
+              ),
+              Expanded(
+                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
+              ),
+              Expanded(
+                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
+              ),
+            ],
+          ),
+        ),
+      ],
+    )
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+    @required this.ThisMargin,
+    @required this.roundedCorners,
+  }) : super(key: key);
+
+  final double ThisMargin;
+  final double roundedCorners;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(ThisMargin),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(roundedCorners),
+        color: Color(0xFF1D1E33),
       ),
     );
   }
