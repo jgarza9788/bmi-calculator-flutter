33533408e015bbc7f36d4fc8b99ca7d245f04605
jgarza9788 <jgarza9788@gmail.com>
2019-09-12 17:31:11 -0700

    custom widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,73 +9,75 @@ class _InputPageState extends State<InputPage> {
   @override
 
   Widget build(BuildContext context) {
-
-    double ThisMargin = 5.0;
-    double roundedCorners = 10.0;
-
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-//      body: Container(
-//        margin: EdgeInsets.all(ThisMargin),
-//        decoration: BoxDecoration(
-//          borderRadius: BorderRadius.circular(20.0),
-//          color: Color(0xFF1D1E33),
-//        ),
-//      ),
-    body: Column(
-      children: <Widget>[
-        Expanded(
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
-              ),
-              Expanded(
-                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
-              ),
-            ],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        Expanded(
-          child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
-        ),
-        Expanded(
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
-              ),
-              Expanded(
-                child: new ReusableCard(ThisMargin: ThisMargin, roundedCorners: roundedCorners),
-              ),
-            ],
+          Expanded(
+            child: ReusableCard(
+              color: Color(0xFF1D1E33),
+            ),
           ),
-        ),
-      ],
-    )
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-    @required this.ThisMargin,
-    @required this.roundedCorners,
-  }) : super(key: key);
+//  const ReusableCard({
+//    Key key,
+//    @required this.ThisMargin,
+//    @required this.roundedCorners,
+//  }) : super(key: key);
 
-  final double ThisMargin;
-  final double roundedCorners;
+  ReusableCard({@required this.color});
+
+  Color color;
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(ThisMargin),
+      margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(roundedCorners),
-        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+        color: color,
       ),
     );
   }
